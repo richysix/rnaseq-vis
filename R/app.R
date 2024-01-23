68cc8a983d74bfba7a35e628a751712d16cef670
Richard White <richard.white@qmul.ac.uk>
2024-01-23 16:44:35 +0000

    Create basic app

--- R/app.R
+++ R/app.R
@@ -1,11 +1,29 @@
 library(shiny)
 
-myApp <- function(...) {
+rnaseqVisApp <- function(...) {
   ui <- fluidPage(
-    ...
+    theme = "flatly.bootstrap.min.css",
+    navbarPage(
+      "geneExpr",
+      tabPanel("Input",
+               sidebarLayout(
+                 sidebarPanel(
+                   uploadRNASeqInput("rnaseq-data"),
+                   width = 3
+                 ),
+                 mainPanel(
+                   tableOutput("samples"),
+                   tableOutput("counts")
+                 )
+               )
+      )
+    )
   )
   server <- function(input, output, session) {
-    ...
+    data_list <- uploadRNASeqServer("rnaseq-data")
+    output$samples <- renderTable(head(data_list$sampleInfo()))
+    output$counts <- renderTable(data_list$counts()[1:5,c(5,7,9:13)])
   }
   shinyApp(ui, server, ...)
 }
+
