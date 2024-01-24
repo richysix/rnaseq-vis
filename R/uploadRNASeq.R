7feb70ae318160a41b21255242f7e5addae57032
Richard White <richard.white@qmul.ac.uk>
2024-01-24 11:43:44 +0000

    Document uploadRNASeq module

--- R/uploadRNASeq.R
+++ R/uploadRNASeq.R
@@ -1,3 +1,21 @@
+#' Create UI components to upload sample and count files
+#'
+#' `uploadRNASeqInput()` produces the buttons for uploading sample and count data 
+#' files. It also provides a checkbox to use the package test data. If sample and
+#' count files are subsequently uploaded, this is used instead of the test data.
+#' 
+#' @param id namespace id for the UI components. Must match the id provided to the 
+#' [uploadRNASeqServer()] function.
+#'
+#' @returns a [htmltools::tagList()] containing two [shiny::fileInput()] controls and 
+#' a [shiny::checkboxInput()].
+#' 
+#' @export
+#'
+#' @examples
+#' 
+#' uploadRNASeqInput("rnaseqData")
+#' 
 uploadRNASeqInput <- function(id) {
   tagList(
     fileInput(NS(id, "sampleFile"), "Sample File"),
@@ -6,6 +24,24 @@ uploadRNASeqInput <- function(id) {
   )
 }
 
+#' Server function to upload sample and count data files
+#'
+#' `uploadRNASeqServer()` implements uploading a sample file and a count data
+#' file. It also handles using the package test data.
+#' 
+#' @param id namespace id for the UI components. Must match the id provided to the 
+#' [uploadRNASeqInput()] function.
+#'
+#' @returns a list containing two [shiny::reactive()] objects
+#' * sampleInfo a data.frame of sample metadata
+#' * counts a data.frame of RNAseq count data
+#' 
+#' @export
+#'
+#' @examples
+#' 
+#' uploadRNASeqServer("rnaseqData")
+#' 
 uploadRNASeqServer <- function(id) {
   moduleServer(id, function(input, output, session) {
     observe({
@@ -53,11 +89,21 @@ uploadRNASeqServer <- function(id) {
 
 }
 
+#' A test shiny app for the uploadRNASeq module
+#'
+#' `uploadRNASeqApp()` creates a small test app for testing the [uploadRNASeqInput()] and
+#' [uploadRNASeqServer()] functions. A subset of the returned sample and count data.frames 
+#' are displayed in two [shiny::tableOutput()]s
+#' 
+#' @return a [shiny::shinyApp()] object
+#'
+#' @examples
+#' uploadRNASeqApp()
 uploadRNASeqApp <- function() {
   ui <- fluidPage(
     sidebarLayout(
       sidebarPanel(
-        uploadRNASeqInput("rnaseq-data")
+        uploadRNASeqInput("rnaseqData")
       ),
       mainPanel(
         tableOutput('samples'),
@@ -67,11 +113,9 @@ uploadRNASeqApp <- function() {
   )
   
   server <- function(input, output, session) {
-    data_list <- uploadRNASeqServer("rnaseq-data")
+    data_list <- uploadRNASeqServer("rnaseqData")
     output$samples <- renderTable(head(data_list$sampleInfo()))
     output$counts <- renderTable(data_list$counts()[1:5,c(5,7,9:13)])
   }
   shinyApp(ui, server)
 } 
-
-uploadRNASeqApp()
