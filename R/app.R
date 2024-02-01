20c8b1d766a47a2fbf8d63cdb463a6bcf27eabf4
Richard White <richard.white@qmul.ac.uk>
2024-02-01 10:43:07 +0000

    Update R/app.R
    Update main app to include subset, transform and cluster modules

--- R/app.R
+++ R/app.R
@@ -4,26 +4,48 @@ rnaseqVisApp <- function(...) {
   ui <- fluidPage(
     theme = "flatly.bootstrap.min.css",
     navbarPage(
-      "geneExpr",
+      "rnaseqVis",
       tabPanel("Input",
                sidebarLayout(
                  sidebarPanel(
-                   uploadRNASeqInput("rnaseq-data"),
+                   uploadRNASeqInput("upload"),
+                   SubsetByGeneIDsInput("subset"),
+                   transformInput("transform"),
+                   clusterInput("cluster"),
                    width = 3
                  ),
                  mainPanel(
-                   tableOutput("samples"),
-                   tableOutput("counts")
+                   SubsetByGeneIDsOutput("subset"),
+                   uploadRNASeqOutput("upload"),
                  )
                )
       )
     )
   )
-  server <- function(input, output, session) {
-    data_list <- uploadRNASeqServer("rnaseq-data")
-    output$samples <- renderTable(head(data_list$sampleInfo()))
-    output$counts <- renderTable(data_list$counts()[1:5,c(5,7,9:13)])
-  }
-  shinyApp(ui, server, ...)
-}
+  server <- function(input, output, session, ...) {
+    extra_args = list(...)
+    debug <- ifelse(is.null(extra_args$debug), FALSE, extra_args$debug)
+    data_list <- uploadRNASeqServer("upload")
+    counts_subset <- SubsetByGeneIDsServer(
+      "subset", 
+      "counts" = reactive(data_list$counts()),
+      "gene_metadata" = reactive(data_list$gene_metadata())
+    )
 
+    # Transform counts
+    transformed_counts <- transformServer("transform", counts = reactive(counts_subset$counts()))
+    
+    # Cluster counts
+    clustered_counts <- clusterServer(
+      "cluster", counts = reactive(transformed_counts()), 
+      gene_metadata = counts_subset$gene_metadata,
+      debug = debug
+    )
+    
+    # Outputs
+    output$samples <- renderTable(head(data_list$sample_info()))
+    output$counts <- renderTable(clustered_counts$counts()[1:5,1:5])
+    output$metadata <- renderTable(clustered_counts$gene_metadata()[1:5,])
+  }
+  shinyApp(ui, server)
+}
