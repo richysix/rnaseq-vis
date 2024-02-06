92f193417de7b058d64d8fbdc184061747318634
Richard White <richard.white@qmul.ac.uk>
2024-02-06 17:04:33 +0000

    Update app with Count Plot tabPanel

--- R/app.R
+++ R/app.R
@@ -25,10 +25,28 @@ rnaseqVisApp <- function(debug = FALSE, ...) {
         "Heatmap",
         heatmapOutput("hmap")
       ),
+      tabPanel(
+        "Count Plot",
+        sidebarLayout(
+          sidebarPanel(
+            countPlotInput('countPlot'),
+            width = 4
+          ),
+          mainPanel(
+            fluidRow(
+              countPlotOutput('countPlot'),
+            ),
+            width = 8
+          )
+        )
+      ),
       tabPanel("Help", includeMarkdown("README.md"))
     )
   )
   server <- function(input, output, session) {
+    # increase max upload size to 200 MB
+    options(shiny.maxRequestSize = 200 * 1024 ^ 2)
+    
     data_list <- uploadRNASeqServer("upload", debug)
     counts_subset <- SubsetByGeneIDsServer(
       "subset", 
@@ -38,20 +56,29 @@ rnaseqVisApp <- function(debug = FALSE, ...) {
     )
 
     # Transform counts
-    transformed_counts <- transformServer("transform", counts = reactive(counts_subset$counts()))
+    transformed_counts <- transformServer("transform", counts = reactive(counts_subset$counts_subset()))
     
     # Cluster counts
     clustered_counts <- clusterServer(
       "cluster", counts = reactive(transformed_counts$counts()), 
-      gene_metadata = reactive(counts_subset$gene_metadata()),
+      gene_metadata = reactive(counts_subset$gene_metadata_subset()),
       debug = debug
     )
     
-    heatmapServer("hmap", counts = reactive(clustered_counts$counts()),
-                  sample_info = reactive(data_list$sample_info()),
-                  gene_metadata = reactive(clustered_counts$gene_metadata()),
-                  transform = reactive(transformed_counts$transform()),
-                  debug = debug)
+    heatmapServer(
+      "hmap", counts = reactive(clustered_counts$counts()),
+      sample_info = reactive(data_list$sample_info()),
+      gene_metadata = reactive(clustered_counts$gene_metadata()),
+      transform = reactive(transformed_counts$transform()),
+      debug = debug
+    )
+    
+    countPlotServer(
+      "countPlot", counts = reactive(counts_subset$counts()), 
+      sample_info = reactive(data_list$sample_info()),
+      gene_metadata = reactive(counts_subset$gene_metadata_subset()),
+      debug = debug
+    )
     
     # Outputs
     output$samples <- renderTable(head(data_list$sample_info()))
